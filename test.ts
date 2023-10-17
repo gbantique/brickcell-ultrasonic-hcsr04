b49bff2be19eb0c828ee37546674c5dc3f36f6de
gbantique <gbantique@gmail.com>
2023-10-17 08:17:09 +0800

    moved main.ts to test.ts for extension purpose

--- test.ts
+++ test.ts
@@ -1,8 +1,8 @@
+let _distance_cm = 0
 serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-    let _distance_cm = Brickcell.getDistance(DigitalPin.P12, DigitalPin.P13);
-
-    serial.writeLine("" + _distance_cm);
-    basic.showNumber(_distance_cm);
+    _distance_cm = Brickcell.getDistance(DigitalPin.P12, DigitalPin.P13)
+    serial.writeLine("" + (_distance_cm))
+    basic.showNumber(_distance_cm)
     basic.pause(2000)
 })
